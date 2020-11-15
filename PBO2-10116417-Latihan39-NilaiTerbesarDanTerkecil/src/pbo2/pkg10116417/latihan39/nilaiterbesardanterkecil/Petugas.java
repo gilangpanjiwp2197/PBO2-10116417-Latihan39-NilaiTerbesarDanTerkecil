6288b34c6ffbfc573f4e11e6557d5de670af6ef3
gilangpanjiwp2197 <73225699+gilangpanjiwp2197@users.noreply.github.com>
2020-11-15 11:02:03 +0700

    Add files via upload

---
+++ PBO2-10116417-Latihan39-NilaiTerbesarDanTerkecil/src/pbo2/pkg10116417/latihan39/nilaiterbesardanterkecil/Petugas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116417.latihan39.nilaiterbesardanterkecil;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Akmal
+ */
+class Petugas {
+    public String namaPetugas;
+    DaftarNilai dafNilai = new DaftarNilai();
+    Scanner scn = new Scanner(System.in);
+
+    public void inputPetugas() {
+        System.out.print("Masukkan Nama Petugas : ");
+        namaPetugas = scn.next();
+    
+} }
